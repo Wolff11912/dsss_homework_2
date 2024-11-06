036adbddc157a612d090b2ca1f662f99d3706807
unknown <constantin.wolff@fau.de>
2024-11-06 15:31:38 +0100

    changed name in setup.py file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="dsss_homework_2",
+    name="math_quiz",
     version="0.1",
     description="Math Quiz",
     author="Constantin Wolff",
